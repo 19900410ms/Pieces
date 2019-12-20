34f5f74672031ea20a0debbb76eff7d5480c9bfe
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-12-20 23:29:55 +0900

    contribution
    投稿・編集機能実装
    image_uploader機能確認

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,15 +1,19 @@
 class ContributionsController < ApplicationController
 
   def index
+    @contributions = Contribution.all
   end
 
   def show
   end
 
   def new
+    @contribution = Contribution.new
   end
 
   def create
+    @contribution = Contribution.create(contribution_parmas)
+    redirect_to root_path
   end
 
   def edit
@@ -21,4 +25,9 @@ class ContributionsController < ApplicationController
   def destroy
   end
   
+  private
+  def contribution_parmas
+    params.require(:contribution).permit(:title, :image, :text).merge(user_id: current_user.id)
+  end
+
 end
