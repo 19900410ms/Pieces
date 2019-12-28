67b9c6206e0792e8fcd559132005ee9a67464d2a
19900410ms <57243729+19900410ms@users.noreply.github.com>
2019-12-28 20:04:05 +0900

    like-03
    非同期通信反応確認

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -6,4 +6,8 @@ class Contribution < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   mount_uploader :image, ImageUploader
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
