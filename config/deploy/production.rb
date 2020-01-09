a55f7bc00167c17e32c2e65cb3ccc5cb7938d385
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-09 12:46:29 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.178.31.31', user: 'ec2-user', roles: %w{app db web}
+server '18.177.202.94', user: 'ec2-user', roles: %w{app db web}
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
 # server-based syntax
